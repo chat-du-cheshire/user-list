bd7c0b2a3400a8505e4864ccf2dc9f1b01c10249
Max Ivanov <maxlvan@yandex.ru>
2019-02-23 12:21:12 +0300

    Random guard

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -7,19 +7,20 @@ import {UserDetailComponent} from './components/user-detail/user-detail.componen
 import {UserCardComponent} from './components/user-card/user-card.component';
 import {Ng2StateDeclaration, Transition, UIRouter, UIRouterModule} from '@uirouter/angular';
 import {UserService} from './services/user.service';
-import {AuthUserService} from '../app-common/services/auth-user.service';
 import {ToastrService} from 'ngx-toastr';
 import {ReactiveFormsModule} from '@angular/forms';
 import {UserFormComponent} from './components/user-form/user-form.component';
 import {UserAddComponent} from './components/user-add/user-add.component';
 import {UserEditComponent} from './components/user-edit/user-edit.component';
 
+const randomBoolean = () => !!Math.floor(Math.random() * 10 % 2);
+
 function requireAuthentication(transition) {
   const $state = transition.router.stateService;
-  const authSvc: AuthUserService = transition.injector().get(AuthUserService);
   const toastr: ToastrService = transition.injector().get(ToastrService);
+  const isAccessGranted = randomBoolean();
 
-  if (authSvc.isAdmin) {
+  if (isAccessGranted) {
     return Promise.resolve(true);
   }
 
